3dfc771e4d16a8965c6ddcdb48f9bb3feabca7ce
Chunsige <dmasl547@gmail.com>
2020-06-16 02:01:39 +0900

    Mainfest  - SignUp : windowSoftInputMode  add (up keyboard - > up button function) SignIn  - add SignUp button, FindPw button In xml  - add function to move Sign up and FindPw in source Add SignUp.xml

---
+++ app/src/main/java/ko/co/rxkotlin/SignUp.kt
@@ -0,0 +1,12 @@
+package ko.co.rxkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUp : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
